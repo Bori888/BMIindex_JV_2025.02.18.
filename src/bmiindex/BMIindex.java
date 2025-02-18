2d15e9c70f37dd356f427bfc337cb3467e25798c
Bori888 <ilyes.bori8@gmail.com>
2025-02-18 16:19:14 +0100

    A sn jo helyre rakva és a kiiras megszebbitese

--- src/bmiindex/BMIindex.java
+++ src/bmiindex/BMIindex.java
@@ -4,69 +4,67 @@ import java.util.Scanner;
 
 public class BMIindex {
 
+    static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         BMIfeladat();
     }
 
-
     public static void BMIfeladat() {
-        kiir("BMI kiszámítás\n");
-        double m = beker("Testmagasság");  
-        double t = beker("Testsúly");     
-        double bmi = bMISzamitas(t, m);  
-        ertekeles(bmi);                   
+        kiir("BMI kiszámítás\n------------------\n");
+        double m = beker("Testmagasság");
+        double t = beker("Testsúly");
+        kiir("--------------------\n");
+        double bmi = bMISzamitas(t, m);
+        ertekeles(bmi);
     }
 
     public static void kiir(String szoveg) {
         System.out.print(szoveg);
     }
 
-  
     public static double beker(String kerdes) {
-        Scanner sc = new Scanner(System.in);
         double adat = 0;
         boolean eldont = false;
-        
+
         while (!eldont) {
             kiir(kerdes + " (100-200 cm között):");
-            adat = sc.nextDouble();  
-            
+            adat = sc.nextDouble();
+
             if (kerdes.equals("Testmagasság")) {
                 if (adat >= 100 && adat <= 200) {
-                    eldont = true;  
+                    eldont = true;
                 } else {
                     kiir("\nHibás adat! Kérjük, adja meg újra a helyes magasságot.\n");
                 }
             } else if (kerdes.equals("Testsúly")) {
                 if (adat >= 40 && adat <= 150) {
-                    eldont = true;  
+                    eldont = true;
                 } else {
                     kiir("\nHibás adat! Kérjük, adja meg újra a helyes testsúlyt.\n");
                 }
             }
         }
-        
-        return adat; 
-    }
 
+        return adat;
+    }
 
     public static double bMISzamitas(double t, double m) {
-        m = m / 100;  
-        return t / (m * m);  
+        m = m / 100;
+        return t / (m * m);
     }
-
 
     public static void ertekeles(double bmi) {
         if (bmi < 20) {
-            kiir("Sovány");
+            kiir("Sovány\n");
         } else if (bmi >= 20 && bmi < 25) {
-            kiir("Normál súlyú");
+            kiir("Normál súlyú\n");
         } else if (bmi >= 25 && bmi < 30) {
-            kiir("Túlsúlyos");
+            kiir("Túlsúlyos\n");
         } else if (bmi >= 30 && bmi < 40) {
-            kiir("Elhízott");
+            kiir("Elhízott\n");
         } else {
-            kiir("Kórosan elhízott");
+            kiir("Kórosan elhízott\n");
         }
     }
 }
